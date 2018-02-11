aba06da9aa19ca0c4678ba422b07438f262743fb
Mayank Chauhan <mayank.chauhan15@gmail.com>
2018-02-11 23:25:57 +0530

    Add accounts app

--- noticeboard/urls.py
+++ noticeboard/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('notices.urls')),
+    path('user/', include('accounts.urls')),
 ]
