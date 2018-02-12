1d048d5d70f5a72bcd7427ed38b8921068fa3073
Mayank Chauhan <mayank.chauhan15@gmail.com>
2018-02-12 23:40:55 +0530

    Add sign up page, more views, more tests and some css.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'accounts'
 
 urlpatterns = [
-		path('signup/', views.signup, name='signup')
+		path('signup/', views.signup, name='signup'),
+		path('logout/', auth_views.LogoutView.as_view(), name='logout')
 		]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('notices:home')
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form': form})
+
